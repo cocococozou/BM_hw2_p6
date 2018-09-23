9fc6fa1afecc42c201e7033f05405015d7b225e2
Xiaoke Zou <xiaokezou@Xiaokes-MacBook-Pro.local>
2018-09-23 01:25:22 -0400

    PART A

--- BM_hw2_p6.R
+++ BM_hw2_p6.R
@@ -25,3 +25,41 @@ length(filter(Migraine_Pos,is.na(CESD))$CESD)
 length(filter(Migraine_Neg,is.na(CESD))$CESD)
 
 ##NDDIE
+##Location and spead
+mean(filter(Migraine_Pos,!is.na(NDDIE))$NDDIE)
+sd(filter(Migraine_Pos,!is.na(NDDIE))$NDDIE)
+mean(filter(Migraine_Neg,!is.na(NDDIE))$NDDIE)
+sd(filter(Migraine_Neg,!is.na(NDDIE))$NDDIE)
+##number of missing values for each variable
+length(filter(Migraine_Pos,is.na(NDDIE))$NDDIE)
+length(filter(Migraine_Neg,is.na(NDDIE))$NDDIE)
+
+##ABNAS memory
+##Location and spead
+mean(filter(Migraine_Pos,!is.na(ABNAS.memory))$ABNAS.memory)
+sd(filter(Migraine_Pos,!is.na(ABNAS.memory))$ABNAS.memory)
+mean(filter(Migraine_Neg,!is.na(ABNAS.memory))$ABNAS.memory)
+sd(filter(Migraine_Neg,!is.na(ABNAS.memory))$ABNAS.memory)
+##number of missing values for each variable
+length(filter(Migraine_Pos,is.na(ABNAS.memory))$ABNAS.memory)
+length(filter(Migraine_Neg,is.na(ABNAS.memory))$ABNAS.memory)
+
+##ABNAS.language
+##Location and spead
+mean(filter(Migraine_Pos,!is.na(ABNAS.language))$ABNAS.language)
+sd(filter(Migraine_Pos,!is.na(ABNAS.language))$ABNAS.language)
+mean(filter(Migraine_Neg,!is.na(ABNAS.language))$ABNAS.language)
+sd(filter(Migraine_Neg,!is.na(ABNAS.language))$ABNAS.language)
+##number of missing values for each variable
+length(filter(Migraine_Pos,is.na(ABNAS.language))$ABNAS.language)
+length(filter(Migraine_Neg,is.na(ABNAS.language))$ABNAS.language)
+
+##cutoff 
+## NDDIE
+length(filter(Migraine_Neg,!is.na(NDDIE),NDDIE>15)$NDDIE)
+length(filter(Migraine_Pos,!is.na(NDDIE),NDDIE>15)$NDDIE)
+##CESD
+length(filter(Migraine_Neg,!is.na(CESD),CESD>15)$CESD)
+length(filter(Migraine_Pos,!is.na(CESD),CESD>15)$CESD)
+
+##
